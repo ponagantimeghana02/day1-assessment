b16a1a2c6077c28a448df16facd9b61b4f1613e1
meghana <ponagantimeghana02@gmail.com>
2026-06-04 12:20:22 +0530

    Displayed types of data

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # day1-assessment
 Day1 assessment on Python Fundamentals + AI Readiness
+
 Created Day1_basics.py file.
 Stored variables as per the task and printed in formatted way.
+
+Printed types of Data through variable assignment.
--- day1_basics.py
+++ day1_basics.py
@@ -12,9 +12,38 @@
 name="meghana"
 age=21
 email="meghana@gmail.com"
-salary=0
+salary=25000.00
 print(f"name is {name}")
 print(f"age is {age}")
 print(f"email is {email}")
 print(f"salary is {salary}")
+
+# Data Types
+# Create examples for:
+
+# str
+# int
+# float
+# bool
+# list
+# tuple
+# dict
+# set
+
+# Print the type of each.
+
+boolean=False
+firstNames=["Megha","Srivya","Niha"]
+lastNames=("Ponaganti","Guntha","Adapa")
+students={"Sanjana":2,"Thrisha":5}
+salaries={10000,2000,4000}
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(boolean))
+print(type(firstNames))
+print(type(lastNames))
+print(type(students))
+print(type(salaries))
 
