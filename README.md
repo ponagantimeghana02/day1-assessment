c6d5bc06ab5644d74d25c26c0ddc6071f8befec9
meghana <ponagantimeghana02@gmail.com>
2026-06-04 12:05:12 +0530

    Display stored variables

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # day1-assessment
 Day1 assessment on Python Fundamentals + AI Readiness
+Created Day1_basics.py file.
+Stored variables as per the task and printed in formatted way.
---
+++ day1_basics.py
@@ -0,0 +1,20 @@
+#Task1
+# Variables
+# Store:
+
+# name
+# age
+# email
+# salary
+
+# Print them in formatted output.
+
+name="meghana"
+age=21
+email="meghana@gmail.com"
+salary=0
+print(f"name is {name}")
+print(f"age is {age}")
+print(f"email is {email}")
+print(f"salary is {salary}")
+
